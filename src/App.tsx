fd277f21bdd6ccf28e3e6d1b6b5cad217562d8ec
cyanxxx <504512494@qq.com>
2022-04-23 16:52:09 +0800

    feat: add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Grid from './components/Grid'
 import Menubar from './components/Menubar'
@@ -12,7 +12,24 @@ import { useTheme } from './hooks/useTheme'
 
 function App() {
   const [colNum, setCol] = useState(COL.ONE)
+  const [auth, setAuth] = useState(false)
   const {theme, setTheme} = useTheme()
+  useEffect(() => {
+    if (!import.meta.env.VITE_SECRET || !import.meta.env.VITE_SECRET_VALUE)return
+    const query = window.location.search
+    if(query) {
+      const keyValue = query.slice(1).split('&')
+      const queryObj = keyValue.reduce((pre: {[key: string]: string}, cur) => {
+        const [key, value] = cur.split('=')
+        pre[key] = value
+        return pre
+      }, {})
+      if (queryObj[`${import.meta.env.VITE_SECRET}`] === import.meta.env.VITE_SECRET_VALUE) setAuth(true)
+    }
+  })
+  if (!auth){
+    return <div>opps...You don't have a right to read this.</div>
+  }
   return (
     <ThemeContext.Provider value={{theme, setTheme}}> 
       <section id='content-container'>
