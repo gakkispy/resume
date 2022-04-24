226ac25be4651a54320ef35dca6be86ce37ea0eb
cyanxxx <504512494@qq.com>
2022-04-24 11:54:06 +0800

    update: resume brief code contribute

--- src/components/Brief.tsx
+++ src/components/Brief.tsx
@@ -19,6 +19,9 @@ export default function Brief() {
              <PartItem>
                 了解Node.js express jest, 有搭建轻量服务端经验，了解基本的CI/CD, 有配置Jenkins和Github Action 项目部署经验
              </PartItem>
+             <PartItem>
+              向file-saver提pull/request解决safair 6.0+不能在新窗口打开并下载，向vue-html-to-paper提pull/reuqeset,解决生成pdf时form表单控件丢失输入输出
+             </PartItem>
          </PartList>
     </div>
   )
